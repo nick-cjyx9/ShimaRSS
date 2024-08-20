1525b0f77ba04ec3a3fa0bf90c4c217b3378b0b3
Nick Chen <i@nickchen.top>
2024-08-20 15:05:12 +0800

    chore: init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Elysia } from "elysia";
+import Container from 'typedi'
+import { Elysia } from 'elysia'
+import { cors } from '@elysiajs/cors'
+import type { Env } from './utils/typedi'
+import app from './controllers/ALL'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export default {
+  async fetch(request: Request, env: Env) {
+    // inject db and env as deps
+    Container.set('env', env)
+    Container.set('db', env.DB)
+    const resp = await new Elysia({ aot: false })
+      .use(cors())
+      .use(app)
+      .handle(request)
+    return resp
+  },
+}
